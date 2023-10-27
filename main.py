cf7d5dcfb2c4eaaadcaa6ee5e9fd79df49252208
Gesh714 <kinegsanchezh@gmail.com>
2023-10-27 13:59:22 +0000

    jueves 261023

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder= 'templates')
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+class AffidavitSigner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    signature = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+    def __repr__(self):
+        return f'<AffidavitSigner {self.name}>'
+
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    with app.app_context():
+        db.create_all()
